59b39bede372f54062d81ab7db3df58c11659643
mirandalu2020 <yuanling1007@hotmail.com>
2023-01-24 14:10:29 -0800

    webiste first draft

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# projects-201
+# projects-201, lab 2
+
+## About Me
+
+This is a project that builds a website that's about me. I explored basic HTML, CSS, and JavaScript on the webiste. 
+
+### Author: Miranda Lu
+
+### Links and Resources
+
+- [HTML Documentation](https://www.w3schools.com/html/default.asp)
+- [CSS Documentation](https://www.w3schools.com/css/default.asp)
+- [JavaScript Documentatin](https://www.w3schools.com/js/default.asp)
+
+### Reflections and Comments
+
+- This website was made without any functions, so it was very tedious to write when there were repeated `if` statements. This project made me really apprecate using functions.
---
+++ js/app.js
@@ -0,0 +1,86 @@
+'use strict';
+console.log('this linked file is working');
+
+let userName = prompt('What\'s your name?');
+//console.log('Welcome to my site, '+userName);
+//console.log('Thank you for joinint me today, ' + userName);
+alert('Welcome to my site, '+userName);
+alert('Thank you for joinint me today, ' + userName);
+
+let doIKnowYou = prompt('Do I know you? Y/N');
+if (doIKnowYou.toLowerCase() === 'yes' || doIKnowYou === 'y') {
+  //console.log('Now I do for sure!') ;
+  alert('Now I do for sure!');
+}
+else if (doIKnowYou.toLowerCase() === 'no' || doIKnowYou === 'n') {
+  console.log('Now I do!');
+  alert('Now I do!');
+}
+else {
+  //console.log('Please answer yes or no');
+  alert('Please answer yes or no');
+}
+
+let doILikeTravel = prompt('Do I like to travel? Y/N');
+if (doILikeTravel.toLowerCase() === 'yes' || doILikeTravel === 'y') {
+  //console.log('You are correct!');
+  alert('You are correct!');
+}
+else if (doILikeTravel.toLowerCase() === 'no' || doILikeTravel === 'n') {
+  //console.log('I actually like traveling sometimes');
+  alert('I actually like traveling sometimes');
+}
+else {
+  alert('Please answer yes or no');
+}
+
+let doILikeBeach = prompt('Do I like beaches? Y/N');
+if (doILikeBeach.toLowerCase() === 'yes' || doILikeBeach === 'y') {
+  //console.log('Not really, I don\'t like salty water');
+  alert('Not really, I don\'t like salty water');
+}
+else if (doILikeBeach.toLowerCase() === 'no' || doILikeBeach === 'n') {
+  //console.log('You are correct!');
+  alert('You are correct!');
+}
+else {
+  //console.log('Please answer yes or no');
+  alert('Please answer yes or no');
+}
+
+let doILikePizza = prompt('Do I like to pizza? Y/N');
+if (doILikePizza.toLowerCase() === 'yes' || doILikePizza === 'y') {
+  //console.log('I actually can survive without pizza!');
+  alert('I actually can survive without pizza!');
+}
+else if (doILikePizza.toLowerCase() === 'no' || doILikePizza === 'n') {
+  //console.log('You are correct!');
+  alert('You are correct!');
+}
+else {
+  //console.log('Please answer yes or no');
+  alert('Please answer yes or no');
+}
+
+let doILikeBaking = prompt('Do I like to bake? Y/N');
+if (doILikeBaking.toLowerCase() === 'yes' || doILikeBaking === 'y') {
+  //console.log('Baking is one of the coolest thing!');
+  alert('Baking is one of the coolest thing!');
+}
+else if (doILikeBaking.toLowerCase() === 'no' || doILikeBaking === 'n') {
+  //console.log('I love baking :}');
+  alert('I love baking :}');
+}
+else {
+  //console.log('Please answer yes or no');
+  alert('Please answer yes or no');
+}
+
+
+
+
+
+
+
+
+
